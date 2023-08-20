c7058bcdfcf8bc1e3c8b457e0c820579d9b16a53
Anushka Srivastava <anushka22086@iiitd.ac.in>
2023-08-21 01:46:23 +0530

    Update loader.c

--- starter/without-bonus/loader.c
+++ starter/without-bonus/loader.c
@@ -5,7 +5,7 @@ Elf32_Phdr *phdr;
 int fd;
 
 // // typedef int (*StartFunction)();
-// int (*_start)(void);
+int (*_start)(void);
 // int (*_start)(void){
 //     entry_addr = (void*)_start;
 //     int(*addr)() = (int(*)())entry_addr;
@@ -54,6 +54,7 @@ void load_and_run_elf(char* exe) {
   while(tmp->p_type != PT_LOAD){
     tmp++;
   }
+  tmp++;
   // if(tmp->p_type == PT_LOAD){
   //   printf("FOUND\n");
   //   // printf("%d\n",tmp->p_type);
@@ -62,7 +63,7 @@ void load_and_run_elf(char* exe) {
   //   printf("NOT FOUND\n");
   // }
   void* virtual_mem = mmap(NULL,tmp->p_memsz,PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE, 0,0);
-  memcpy(virtual_mem,ehdr+tmp->p_offset,tmp->p_memsz);
+  memcpy(virtual_mem,heap_mem+tmp->p_offset,tmp->p_memsz);
 
   //  for (ssize_t i = 0; i < tmp->p_memsz; i++) {
   //     printf("%02x ", ((unsigned char*)virtual_mem)[i]);
@@ -73,9 +74,30 @@ void load_and_run_elf(char* exe) {
 
   void* entry_addr = virtual_mem + (entry - tmp->p_vaddr);
   printf("Entry Point Address in Memory: %p\n", entry_addr);
-  close(fd);
+  printf("Virtual mem start: %p\n", virtual_mem);
+  printf("Virtual mem end: %p\n", virtual_mem + tmp->p_memsz);
+  // close(fd);
 
-  
+  // if(entry_addr != NULL) {
+  //   _start = (int (*)(void))entry_addr;
+  //   // ret = sum(1, 2);
+  //   // printf("sum:%d\n", ret);
+  //   int result = _start();
+  //   printf("User _start return value = %d\n",result);
+  // };
+
+  if (entry_addr >= virtual_mem && entry_addr < virtual_mem + tmp->p_memsz) {
+    printf("Entry Point Address in Memory: %p\n", entry_addr);
+
+    if (entry_addr != NULL) {
+        _start = (int (*)(void))entry_addr;
+      // printf("yes\n");
+        int result = _start();
+        printf("User _start return value = %d\n", result);
+    }
+} else {
+    printf("Entry Point Address is out of bounds.\n");
+}
 
   // 1. Load entire binary content into the memory from the ELF file.
   // 2. Iterate through the PHDR table and find the section of PT_LOAD 
