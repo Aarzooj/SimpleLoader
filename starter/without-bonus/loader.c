9212d8dfd0abf4c07eac918158d5a31dbf7a9655
Anushka Srivastava <anushka22086@iiitd.ac.in>
2023-08-22 00:23:07 +0530

    Error handling and adding comments

--- starter/without-bonus/loader.c
+++ starter/without-bonus/loader.c
@@ -9,6 +9,7 @@ int fd;
  */
 void loader_cleanup()
 {
+  // freeing the memory for globally defined variables
   ehdr = NULL;
   phdr = NULL;
   free(ehdr);
@@ -20,78 +21,6 @@ void loader_cleanup()
  */
 void load_and_run_elf(char *exe)
 {
-  fd = open(exe, O_RDONLY);
-
-  off_t fd_size = lseek(fd, 0, SEEK_END);
-  lseek(fd, 0, SEEK_SET);
-  // printf("File size: %lld\n", (long long)fd_size);
-
-  char *heap_mem;
-  heap_mem = (char *)malloc(fd_size);
-  // printf("Heap Memory Allocated\n");
-
-  ssize_t file_read = read(fd, heap_mem, fd_size);
-  // printf("File reading Done\n");
-  // printf("Address of heap: %p\n", (void*)heap_mem);
-
-  ehdr = (Elf32_Ehdr *)heap_mem;
-  // printf("Address of ehdr: %p\n", (void*)ehdr);
-
-  phdr = (Elf32_Phdr *)(heap_mem + ehdr->e_phoff);
-  // printf("offset: %d\n", ehdr->e_phoff);
-  // printf("Address of phdr: %p\n", (void*)phdr);
-
-  unsigned int entry = ehdr->e_entry;
-  // printf("Entry Point address is %#x\n",entry);
-  // printf("Program Headers: %d\n", ehdr->e_phnum);
-  // printf("size of Program Headers: %d\n", ehdr->e_phentsize);
-  // printf("Offset of Program Headers: %d\n", ehdr->e_phoff);
-  Elf32_Phdr *tmp = phdr;
-  int total_phdr = ehdr->e_phnum;
-  void *virtual_mem;
-  void *entry_addr;
-  int i = 0;
-  while (i < total_phdr)
-  {
-    if (tmp->p_type == PT_LOAD)
-    {
-      virtual_mem = mmap(NULL, tmp->p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
-      memcpy(virtual_mem, heap_mem + tmp->p_offset, tmp->p_memsz);
-      entry_addr = virtual_mem + (entry - tmp->p_vaddr);
-      if (entry_addr > virtual_mem && entry_addr < (virtual_mem + tmp->p_offset))
-      {
-        break;
-      }
-    }
-    tmp++;
-  }
-
-  //  for (ssize_t i = 0; i < tmp->p_memsz; i++) {
-  //     printf("%02x ", ((unsigned char*)virtual_mem)[i]);
-  //     if ((i + 1) % 16 == 0) {
-  //         printf("\n");
-  //     }
-  // }
-
-  // printf("Entry Point Address in Memory: %p\n", entry_addr);
-  // printf("Virtual mem start: %p\n", virtual_mem);
-  // printf("Virtual mem end: %p\n", virtual_mem + tmp->p_memsz);
-  // close(fd);
-
-  //   if (entry_addr >= virtual_mem && entry_addr < virtual_mem + tmp->p_memsz) {
-  //     printf("Entry Point Address in Memory: %p\n", entry_addr);
-
-  if (entry_addr != NULL)
-  {
-    int (*_start)(void) = (int (*)(void))entry_addr;
-    int result = _start();
-    printf("User _start return value = %d\n", result);
-  }
-  else
-  {
-    printf("Entry Point Address is out of bounds.\n");
-  }
-
   // 1. Load entire binary content into the memory from the ELF file.
   // 2. Iterate through the PHDR table and find the section of PT_LOAD
   //    type that contains the address of the entrypoint method in fib.c
@@ -101,8 +30,98 @@ void load_and_run_elf(char *exe)
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
   // 6. Call the "_start" method and print the value returned from the "_start"
 
-  // int result = _start();
-  // printf("User _start return value = %d\n",result);
+  // 1. reading the file and loading it into memory
+  fd = open(exe, O_RDONLY);
+
+  off_t fd_size = lseek(fd, 0, SEEK_END);
+  lseek(fd, 0, SEEK_SET);
+
+  char *heap_mem;
+  heap_mem = (char *)malloc(fd_size);
+
+  // verifying if memory is allocated
+  if (!heap_mem)
+  {
+    perror("Error: Memory allocation failed");
+    exit(1);
+  }
+
+  ssize_t file_read = read(fd, heap_mem, fd_size);
+
+  // verifying if file is read successfully
+  if (file_read < 0 || (size_t)file_read != fd_size)
+  {
+    perror("Error: File read operation failed");
+    free(heap_mem);
+    exit(1);
+  }
+
+  // elf header
+  ehdr = (Elf32_Ehdr *)heap_mem;
+
+  // checking if the file type can be handled by the loader
+  if (ehdr->e_type != ET_EXEC)
+  {
+    printf("Unsupported elf file");
+    exit(1);
+  }
+  // program header
+  phdr = (Elf32_Phdr *)(heap_mem + ehdr->e_phoff);
+
+  // entrypoint address in elf header
+  unsigned int entry = ehdr->e_entry;
+
+  Elf32_Phdr *tmp = phdr;
+  int total_phdr = ehdr->e_phnum;
+  void *virtual_mem;
+  void *entry_addr;
+  int i = 0;
+
+  // 2. Iterating through phdr to find the section of the PT_LOAD containing the entrypoint address
+  while (i < total_phdr)
+  {
+    if (tmp->p_type == PT_LOAD)
+    {
+      // 3. loading the segment in virtual memory
+      virtual_mem = mmap(NULL, tmp->p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+      memcpy(virtual_mem, heap_mem + tmp->p_offset, tmp->p_memsz);
+
+      //verifying if memory mapping was successful
+      if (virtual_mem == MAP_FAILED)
+      {
+        perror("Error: Memory mapping failed");
+        free(heap_mem);
+        exit(1);
+      }
+
+      // 4. entry point address in the virtual memory
+      entry_addr = virtual_mem + (entry - tmp->p_vaddr);
+
+      // checking if the entry point address lies within the boundaries of virtual memory
+      if (entry_addr > virtual_mem && entry_addr < (virtual_mem + tmp->p_offset))
+      {
+        break;
+      }
+    }
+    tmp++;
+  }
+
+  if (entry_addr != NULL)
+  {
+    // 5. typecasting the address into the function pointer
+    int (*_start)(void) = (int (*)(void))entry_addr;
+
+    // 6. printing the result
+    int result = _start();
+    printf("User _start return value = %d\n", result);
+  }
+  else
+  {
+    // error handling if the entry point is out of bounds
+    printf("Entry Point Address is out of bounds.\n");
+    free(heap_mem);
+    exit(1);
+  }
 }
 
 int main(int argc, char **argv)
@@ -113,9 +132,21 @@ int main(int argc, char **argv)
     exit(1);
   }
   // 1. carry out necessary checks on the input ELF file
+
+  // checking if ELF file exists in the provided path
+  FILE *elfFile = fopen(argv[1], "rb");
+  if (!elfFile)
+  {
+    printf("Error: Unable to open ELF file.\n");
+    exit(1);
+  }
+  fclose(elfFile);
+
   // 2. passing it to the loader for carrying out the loading/execution
   load_and_run_elf(argv[1]);
+
   // 3. invoke the cleanup routine inside the loader
   loader_cleanup();
+
   return 0;
 }
